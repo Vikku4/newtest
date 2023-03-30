fad9b1c676e30e3290ca6772131f1ae901e6283c
Vikku4 <vikraamsuresh@gmail.com>
2023-03-30 12:57:05 +0530

    new file for developbranch

---
+++ switch.js
@@ -0,0 +1,10 @@
+let day = "Wednesday"
+
+switch(day) {
+    case 'Monday': 
+        console.log("start of the week");
+    case 'Tuesday': 
+        console.log("2nd day of the week");
+    case 'Wednesday': 
+        console.log("3rd day of the week");
+}
